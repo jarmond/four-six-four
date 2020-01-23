bf9df776ee844a956c7794f4b052d5c89e515a84
Jonathan Armond <jwarmond@outlook.com>
2020-01-23 00:18:57 +0000

    Break out debugger loop

--- src/four_six_four/z80/control.clj
+++ src/four_six_four/z80/control.clj
@@ -29,6 +29,36 @@
        (inc-refresh))
       (operation (:instr decode)))))
 
+(defn debugger-loop
+  "User interface loop for debugging during stepwise execution."
+  [decode]
+  (loop []
+    (newline)
+    (println (format-decode (merge decode {:loc (get-pc)})))
+    (print "?> ")
+    (flush)
+    (case (first (read-line))
+      ;; execute next
+      \n nil
+      ;; quit
+      \q (do
+           (println "quitting")
+           (dosync
+            (toggle-running))
+           (print-z80 true)
+           :quit)
+      ;; print z80 state
+      \p (do
+           (print-z80 true)
+           (recur))
+      ;; print IR
+      \i (do
+           (print decode)
+           (recur))
+      (do
+        (print "invalid command")
+        (recur)))))
+
 
 (defn execute-step
   "Execute bound VM running program from `start` in memory step-wise. Runs until
@@ -40,28 +70,10 @@
    (set-running))
   (while (is-running)
     (let [decode (fetch-instruction)]
-      (loop []
-        (newline)
-        (println (format-decode (merge decode {:loc (get-pc)})))
-        (print "?> ")
-        (flush)
-        (case (first (read-line))
-          \n (do
-               (dosync
-                (inc-refresh))
-               (operation (:instr decode)))
-          \q (do
-               (println "quitting")
-               (dosync
-                (toggle-running)
-                (print-z80 true)))
-          \p (do
-               (print-z80 true)
-               (recur))
-          \i (do
-               (print decode)
-               (recur))
-          (print "invalid command"))))))
+      (when-not (= (debugger-loop decode) :quit)
+        (dosync
+         (inc-refresh))
+        (operation (:instr decode))))))
 
 (defn execute-program
   "Execute bound VM running `program` copied into location `start`."
